f50250f0018e2755db9f4036663a8303f8dc2001
Eric <e.bowman25@gmail.com>
2015-10-19 00:29:44 -0500

    Initial Comit

---
+++ StringAssessment/StringAssessment/StringAssessment.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace StringAssessment
+{
+    partial class StringAssessment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input";
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(13, 30);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(669, 136);
+            this.InputTextBox.TabIndex = 1;
+            this.InputTextBox.Text = "Write sentence here...";
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(13, 369);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ReadOnly = true;
+            this.OutputTextBox.Size = new System.Drawing.Size(669, 136);
+            this.OutputTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 353);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // StringAssessment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 517);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "StringAssessment";
+            this.Text = "StringAssessment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ StringAssessment/StringAssessment/StringAssessment.cs
@@ -0,0 +1,99 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////
+/// Author: Eric Bowman
+/// Date Octorber 18th, 2014
+/// 
+/// 
+/// This application parses a sentence and replaces each word with the following: 
+/// first letter, number of distinct characters between first and last character, and last letter.  
+/// 
+/// Button: 'Run' will run parse the input and display the results in the output.
+/// Button: 'Clear' will clear both text boxes.
+///  
+/// 
+////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace StringAssessment
+{
+    public partial class StringAssessment : Form
+    {
+        public StringAssessment()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            InputTextBox.Clear();
+            OutputTextBox.Clear();
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            var tmp = ParseInputString(InputTextBox.Text);
+            OutputTextBox.Text = tmp;
+        }
+
+
+        private string ParseInputString(string inputString)
+        {
+            if (inputString == "")
+                return inputString;
+
+            var tempCharArray = inputString.ToCharArray();
+            var regExLetter = new Regex("[^A-Za-z]");
+            var regExNonSpaceChar = new Regex("[^\\S]");
+             
+            var indexList = new List<int>();
+            for (var i = 0; i < inputString.Length; i++)
+            {
+                if (regExNonSpaceChar.IsMatch(tempCharArray[i].ToString()) ||
+                    regExLetter.IsMatch(tempCharArray[i].ToString()))
+                {
+                    indexList.Add(i);
+                }
+            }
+
+            if (indexList.Count == 0) return GetNewString(inputString);
+
+            //Get first substring
+            var outputText = GetNewString(inputString.Substring(0, indexList[0]));
+            //Get first non letter
+            outputText = outputText + inputString.Substring(indexList[0], 1);
+
+            for (var i = 0; i < indexList.Count - 1; i++)
+            {
+                var index = indexList[i] + 1;
+                var length = indexList[i + 1] - indexList[i] - 1;
+                var newOutput = GetNewString(inputString.Substring(index, length));
+                outputText = outputText + newOutput + inputString.Substring(index + length, 1);
+            }
+
+            //Get last substring if it's a letter
+            if ( indexList[indexList.Count - 1] + 1 != inputString.Length)
+                outputText = outputText + GetNewString(inputString.Substring(indexList[indexList.Count - 1]));
+
+
+            return outputText;
+        }
+
+
+        private string GetNewString(string inputString)
+        {
+            if (inputString.Length < 2) return inputString;
+            var innerString = inputString.Substring(1, inputString.Length - 2);
+            var tempCharArray = innerString.ToCharArray();
+            tempCharArray = tempCharArray.Distinct().ToArray();
+
+            var ouputString = inputString.Substring(0, 1) + tempCharArray.Length +
+                              inputString.Substring(inputString.Length - 1, 1);
+
+            return ouputString;
+        }
+    }
+}
